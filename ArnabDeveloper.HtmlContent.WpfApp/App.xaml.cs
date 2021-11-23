17fbbb69b0e9ceae232ddbc52746737e52d5b951
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-11-23 13:52:28 +0530

    dotnet 6 upgrade

--- ArnabDeveloper.HtmlContent.WpfApp/App.xaml.cs
+++ ArnabDeveloper.HtmlContent.WpfApp/App.xaml.cs
@@ -1,34 +1,32 @@
 ﻿using ArnabDeveloper.HtmlContent.Core.Services;
 using ArnabDeveloper.HttpHealthCheck.DI;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Windows;
 
-namespace ArnabDeveloper.HtmlContent.WpfApp
+namespace ArnabDeveloper.HtmlContent.WpfApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private MainWindow? _window;
+
+    protected override void OnStartup(StartupEventArgs e)
     {
-        private MainWindow? _window;
+        IServiceCollection services = new ServiceCollection();
+        services.AddHtmlContentService();
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        IHtmlContentService htmlContentService = serviceProvider.GetRequiredService<IHtmlContentService>();
+        _window = new(htmlContentService);
+        _window.Show();
+    }
 
-        protected override void OnStartup(StartupEventArgs e)
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_window != null)
         {
-            IServiceCollection services = new ServiceCollection();
-            services.AddHtmlContentService();
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-            IHtmlContentService htmlContentService = serviceProvider.GetRequiredService<IHtmlContentService>();
-            _window = new(htmlContentService);
-            _window.Show();
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            if (_window != null)
-            {
-                _window.Close();
-            }
+            _window.Close();
         }
     }
 }
--- ArnabDeveloper.HtmlContent.WpfApp/MainWindow.xaml.cs
+++ ArnabDeveloper.HtmlContent.WpfApp/MainWindow.xaml.cs
@@ -1,129 +1,126 @@
 ﻿using ArnabDeveloper.HtmlContent.Core.Models;
 using ArnabDeveloper.HtmlContent.Core.Services;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 
-namespace ArnabDeveloper.HtmlContent.WpfApp
+namespace ArnabDeveloper.HtmlContent.WpfApp;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private readonly IHtmlContentService _htmlContentService;
+
+    public MainWindow(IHtmlContentService htmlContentService)
     {
-        private readonly IHtmlContentService _htmlContentService;
+        InitializeComponent();
+        _htmlContentService = htmlContentService;
+        AddUrls();
+    }
 
-        public MainWindow(IHtmlContentService htmlContentService)
+    private void BtnGetContent_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        IEnumerable<WebSiteDataModel> webSiteDataModels = _htmlContentService.GetContent();
+        stopwatch.Stop();
+
+        PrintData(webSiteDataModels);
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private async void BtnGetContentAsync_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService.GetContentAsync();
+        stopwatch.Stop();
+
+        PrintData(webSiteDataModels);
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private async void BtnGetContentAsyncStream_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        await foreach (ProgressDataModel progressDataModel
+            in _htmlContentService.GetContentAsyncStream())
         {
-            InitializeComponent();
-            _htmlContentService = htmlContentService;
-            AddUrls();
-        }
-
-        private void BtnGetContent_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = string.Empty;
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            IEnumerable<WebSiteDataModel> webSiteDataModels = _htmlContentService.GetContent();
-            stopwatch.Stop();
-
-            PrintData(webSiteDataModels);
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
-        }
-
-        private async void BtnGetContentAsync_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = string.Empty;
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService.GetContentAsync();
-            stopwatch.Stop();
-
-            PrintData(webSiteDataModels);
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
-        }
-
-        private async void BtnGetContentAsyncStream_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = string.Empty;
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            await foreach (ProgressDataModel progressDataModel
-                in _htmlContentService.GetContentAsyncStream())
+            prg.Value = progressDataModel.ProgressValue;
+            if (progressDataModel.Data != null)
             {
-                prg.Value = progressDataModel.ProgressValue;
-                if (progressDataModel.Data != null)
-                {
-                    txtResult.Text += $"{progressDataModel.Data.WebsiteUrl} {progressDataModel.Data.WebsiteData.Length}\n";
-                }
+                txtResult.Text += $"{progressDataModel.Data.WebsiteUrl} {progressDataModel.Data.WebsiteData.Length}\n";
             }
-            stopwatch.Stop();
-
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
         }
+        stopwatch.Stop();
 
-        private async void BtnGetContentParallelAsync_Click(object sender, RoutedEventArgs e)
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private async void BtnGetContentParallelAsync_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService.GetContentParallelAsync();
+        stopwatch.Stop();
+
+        PrintData(webSiteDataModels);
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private async void BtnGetContentParallelForEachAsync_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService
+            .GetContentParallelForEachAsync();
+        stopwatch.Stop();
+
+        PrintData(webSiteDataModels);
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private async void BtnGetContentParallelForEachProgressAsync_Click(object sender, RoutedEventArgs e)
+    {
+        txtResult.Text = string.Empty;
+        Progress<ProgressDataModel> progress = new(progressDataModel =>
         {
-            txtResult.Text = string.Empty;
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService.GetContentParallelAsync();
-            stopwatch.Stop();
-
-            PrintData(webSiteDataModels);
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
-        }
-
-        private async void BtnGetContentParallelForEachAsync_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = string.Empty;
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService
-                .GetContentParallelForEachAsync();
-            stopwatch.Stop();
-
-            PrintData(webSiteDataModels);
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
-        }
-
-        private async void BtnGetContentParallelForEachProgressAsync_Click(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = string.Empty;
-            Progress<ProgressDataModel> progress = new(progressDataModel =>
+            prg.Value = progressDataModel.ProgressValue;
+            if (progressDataModel.Data != null)
             {
-                prg.Value = progressDataModel.ProgressValue;
-                if (progressDataModel.Data != null)
-                {
-                    txtResult.Text += $"{progressDataModel.Data.WebsiteUrl} {progressDataModel.Data.WebsiteData.Length}\n";
-                }
-            });
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService
-                .GetContentParallelForEachProgressAsync(progress);
-            stopwatch.Stop();
-
-            txtResult.Text += stopwatch.ElapsedMilliseconds;
-        }
-
-        private void AddUrls()
-        {
-            _htmlContentService.Urls.Add("http://google.com");
-            _htmlContentService.Urls.Add("http://microsoft.com");
-            _htmlContentService.Urls.Add("http://github.com");
-            _htmlContentService.Urls.Add("http://bitbucket.com");
-            _htmlContentService.Urls.Add("http://gmail.com");
-            _htmlContentService.Urls.Add("http://office.com");
-            _htmlContentService.Urls.Add("http://outlook.com");
-            _htmlContentService.Urls.Add("http://www.businessinsider.com");
-        }
-
-        private void PrintData(IEnumerable<WebSiteDataModel> webSiteDataModels)
-        {
-            foreach (WebSiteDataModel webSiteDataModel in webSiteDataModels)
-            {
-                txtResult.Text += $"{webSiteDataModel.WebsiteUrl} {webSiteDataModel.WebsiteData.Length}\n";
+                txtResult.Text += $"{progressDataModel.Data.WebsiteUrl} {progressDataModel.Data.WebsiteData.Length}\n";
             }
+        });
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        IEnumerable<WebSiteDataModel> webSiteDataModels = await _htmlContentService
+            .GetContentParallelForEachProgressAsync(progress);
+        stopwatch.Stop();
+
+        txtResult.Text += stopwatch.ElapsedMilliseconds;
+    }
+
+    private void AddUrls()
+    {
+        _htmlContentService.Urls.Add("http://google.com");
+        _htmlContentService.Urls.Add("http://microsoft.com");
+        _htmlContentService.Urls.Add("http://github.com");
+        _htmlContentService.Urls.Add("http://bitbucket.com");
+        _htmlContentService.Urls.Add("http://gmail.com");
+        _htmlContentService.Urls.Add("http://office.com");
+        _htmlContentService.Urls.Add("http://outlook.com");
+        _htmlContentService.Urls.Add("http://timesofindia.indiatimes.com");
+    }
+
+    private void PrintData(IEnumerable<WebSiteDataModel> webSiteDataModels)
+    {
+        foreach (WebSiteDataModel webSiteDataModel in webSiteDataModels)
+        {
+            txtResult.Text += $"{webSiteDataModel.WebsiteUrl} {webSiteDataModel.WebsiteData.Length}\n";
         }
     }
 }
